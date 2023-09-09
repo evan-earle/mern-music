a66eebb8daab530ccbc03c33e7e006a84195814e
Evan Earle <evan.earle@gmail.com>
2023-09-09 14:25:00 -0300

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
