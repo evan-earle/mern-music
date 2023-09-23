432d18147fbd523594adc2c0969d107ac008726d
Evan Earle <evan.earle@gmail.com>
2023-09-23 10:34:00 -0300

    configured App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Auth } from "./pages/Auth";
+import { Search } from "./pages/Search";
+import { Home } from "./pages/Home";
+import { EditProfile } from "./pages/EditProfile";
+import { Toaster } from "react-hot-toast";
+import PrivateRoutes from "./components/PrivateRoutes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Toaster
+        position="top-left"
+        toastOptions={{
+          style: {
+            fontSize: "1.5rem",
+          },
+        }}
+      ></Toaster>
+      <Router>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/edit-profile" element={<EditProfile />} />
+          </Route>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
