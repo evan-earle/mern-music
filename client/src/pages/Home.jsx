71d391aa036d4b9255169b1323a5b13a5c296b38
Evan Earle <evan.earle@gmail.com>
2023-10-06 12:56:00 -0300

    added get artist functions

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,24 +3,30 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const Home = () => {
+  const [artist, setArtist] = useState("");
+
+  const getArtistfromDB = async () => {
+    try {
+      const lastSearch = await axios.get(`/api/users/lastSearch`);
+      console.log(lastSearch);
+      const artist = lastSearch.data.lastSearch;
+      getArtist(artist);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const getArtist = async (artist) => {
     try {
-      if (artist) {
-        const data = await axios.get(`/api/music/artist/${artist}`);
-        console.log(data);
-      } else {
-        const lastSearch = await axios.get(`/api/users/lastSearch`);
-
-        const data = await axios.get(`/api/music/artist/${lastSearch}`);
-        console.log(data);
-      }
+      const data = await axios.get(`/api/music/artist/${artist}`);
+      console.log(data);
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    getArtist();
+    getArtistfromDB();
   }, []);
 
   return (
